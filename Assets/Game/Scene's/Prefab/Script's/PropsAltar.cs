944c90c5a69c042e936796c786ec757509c5ddfd
Mach1n1 <starshinin98@mail.ru>
2022-08-07 15:33:47 +0900

    Switching maps

---
+++ Assets/Game/Scene's/Prefab/Script's/PropsAltar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsAltar : MonoBehaviour
+{
+    [SerializeField] private GameObject _MAPArena;
+    [SerializeField] private GameObject _MAPBase;
+
+    public List<SpriteRenderer> runes;
+    public float lerpSpeed;
+
+    private Color curColor;
+    private Color targetColor;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        targetColor = new Color(1, 1, 1, 1);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        targetColor = new Color(1, 1, 1, 0);
+        _MAPArena.SetActive(true);
+        _MAPBase.SetActive(false); 
+    }
+    private void Update()
+    {
+        curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+
+        foreach (var r in runes)
+        {
+           r.color = curColor;
+
+        }
+    }
+}
